cfc51135de7dd7b168b6b81bdfb16a06bf7f87a9
unknown <aditya.91.singh@gmail.com>
2019-03-03 17:08:43 +0000

    added 3 classifiers for location company and goodtype

--- Classifier.py
+++ Classifier.py
@@ -9,49 +9,11 @@ from tpot import TPOTClassifier
 import tpot
 
 class Classifier:
-    def load_models(self,path):
-        try:
-            self.svc = pickle.load(open(path+'svm.sav','rb'))
-            self.rf = pickle.load(open(path+'rf.sav','rb'))
-            self.xgb = pickle.load(open(path+'xgb.sav','rb'))
-            self.knn = pickle.load(open(path+'knn.sav','rb'))
-        except:
-            raise Exception('Missing saved files in path. Train classifiers first')
-
-    def train_classifiers(self,X_train,y_train,X_test, y_test,save_path):
-            print('Training...')
-
-            print('SVM')
-            self.svc = SVC(gamma='auto')
-            self.svc.fit(X_train,y_train)
-
-            print('Random Forest')
-            self.rf = RandomForestClassifier(n_estimators=150,max_depth=5,random_state=0)
-            self.rf.fit(X_train,y_train)
-
-            print('Gradient Boosting')
-            self.xgb = GradientBoostingClassifier(n_estimators=150,max_depth=5,random_state=0)
-            self.xgb.fit(X_train,y_train)
-
-            print('K Nearest Neighbours')
-            self.knn = KNeighborsClassifier(n_neighbors=7)
-            self.knn.fit(X_train,y_train)
-
-            print('Test accuracies')
-            print('SVC:',self.svc.score(X_test,y_test))
-            print('RF:',self.rf.score(X_test,y_test))
-            print('XGB:',self.xgb.score(X_test,y_test))
-            print('KNN:',self.knn.score(X_test,y_test))
-
-            pickle.dump(self.svc, open(save_path+'svc.sav', 'wb'))
-            pickle.dump(self.rf, open(save_path+'rf.sav', 'wb'))
-            pickle.dump(self.xgb, open(save_path+'xgb.sav', 'wb'))
-            pickle.dump(self.knn, open(save_path+'knn.sav', 'wb'))
 
     def tpot_classifiers(self,X_train,y_train,X_test, y_test,save_path):
         print('Training using Tpot')
-        pipeline_optimizer = TPOTClassifier(generations=5, population_size=20, cv=3,
+        pipeline_optimizer = TPOTClassifier(generations=5, population_size=50, cv=3,
                                             random_state=0, verbosity=2,scoring='balanced_accuracy')
         pipeline_optimizer.fit(X_train, y_train)
-        pipeline_optimizer.export(save_path+'tpot.py')
+        pipeline_optimizer.export(save_path+'.py')
         print(pipeline_optimizer.score(X_test, y_test))
---
+++ models/one_vs_rest_location.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import make_pipeline, make_union
+from tpot.builtins import StackingEstimator
+
+# NOTE: Make sure that the class is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1).values
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'].values, random_state=0)
+
+# Average CV score on the training set was:0.6161974737245192
+exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=RandomForestClassifier(bootstrap=False, criterion="entropy", max_features=0.25, min_samples_leaf=9, min_samples_split=10, n_estimators=100)),
+    StackingEstimator(estimator=RandomForestClassifier(bootstrap=True, criterion="gini", max_features=0.5, min_samples_leaf=7, min_samples_split=8, n_estimators=100)),
+    KNeighborsClassifier(n_neighbors=29, p=2, weights="distance")
+)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
